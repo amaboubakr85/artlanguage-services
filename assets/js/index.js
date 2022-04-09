ec169255b616d9711b6518d1841b3c8f6a449786
Ahmed Aboubakr <a.m.aboubakr85@gmail.com>
2022-04-09 21:19:21 +0300

    finalize

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,69 @@
-let play = document.getElementById('play')
-let plays = document.querySelectorAll('.plays')
+let play = document.getElementById("play");
+let plays = document.querySelectorAll(".plays");
 if (plays.length > 0) {
-  //   play.addEventListener('click', function () {
-  //     let play_video = document.querySelector('.video-play')
-  //     document.querySelector('.overlay').style.opacity = '0'
-  //     document.querySelector('.overlay').style.zIndex = '0'
-  //     play_video.play()
-  //     play_video.controls = true
-  //   })
-
   plays.forEach((item) => {
-    item.addEventListener('click', (e) => {
-      item.parentElement.style.opacity = '0'
-      item.parentElement.style.zIndex = '0'
-      let playV = item.parentElement.nextElementSibling
+    item.addEventListener("click", (e) => {
+      item.parentElement.style.opacity = "0";
+      item.parentElement.style.zIndex = "0";
+      let playV = item.parentElement.nextElementSibling;
 
-      playV.play()
-      playV.controls = true
-    })
-  })
+      playV.play();
+      playV.controls = true;
+    });
+  });
 }
+
+let prices = document.querySelectorAll(".prices .links ul li");
+prices.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    // item.sibl.classList.remove('active')
+    // item.classList.add('active')
+    [...item.parentElement.children].forEach((sib) =>
+        sib.classList.remove("active")
+    );
+    item.classList.add("active");
+    let cat = item.getAttribute("data-class");
+    console.log(cat);
+    let videos = document.querySelector(".videos-price");
+    let photos = document.querySelector(".photos-price");
+    if (cat == ".videos-price") {
+      photos.style.display = "none";
+      videos.style.display = "block";
+    } else {
+      photos.style.display = "block";
+      videos.style.display = "none";
+    }
+  });
+});
+
+let albums = document.querySelectorAll(".albums .links ul li");
+if (albums) {
+  albums.forEach((item) => {
+    item.addEventListener("click", function () {
+      [...item.parentElement.children].forEach((sib) =>
+          sib.classList.remove("active")
+      );
+      item.classList.add("active");
+      let cat = item.getAttribute("data-class");
+      console.log(cat);
+      let allImages = document.querySelectorAll(".albums .albums-images");
+      allImages.forEach((item) => {
+        item.classList.add("d-none");
+      });
+      document
+          .querySelector(`.albums .albums-images${cat}`)
+          .classList.remove("d-none");
+      document
+          .querySelector(`.albums .albums-images${cat}`)
+          .classList.add("d-grid");
+    });
+  });
+}
+
+let allImages = document.querySelectorAll(".albums .albums-images");
+allImages.forEach(item => {
+  item.classList.add('d-none');
+
+})
+document.querySelector(`.albums .albums-images.foods`).classList.remove('d-none');
+document.querySelector(`.albums .albums-images.foods`).classList.add('d-grid');
