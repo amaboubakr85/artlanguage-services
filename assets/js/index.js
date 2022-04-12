b34224645c54ce191ebaa29e83c6f9a3329a7d87
amaboubakr85 <a.m.aboubakr85@gmail.com>
2022-04-12 19:51:18 +0300

    zoom css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,70 +1,91 @@
 let play = document.getElementById("play");
 let plays = document.querySelectorAll(".plays");
 if (plays.length > 0) {
-  plays.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      item.parentElement.style.opacity = "0";
-      item.parentElement.style.zIndex = "0";
-      let playV = item.parentElement.nextElementSibling;
+    plays.forEach((item) => {
+        item.addEventListener("click", (e) => {
+            item.parentElement.style.opacity = "0";
+            item.parentElement.style.zIndex = "0";
+            let playV = item.parentElement.nextElementSibling;
 
-      playV.play();
-      playV.controls = true;
+            playV.play();
+            playV.controls = true;
+        });
     });
-  });
 }
 
 let prices = document.querySelectorAll(".prices .links ul li");
 prices.forEach((item) => {
-  item.addEventListener("click", function (e) {
-    // item.sibl.classList.remove('active')
-    // item.classList.add('active')
-    [...item.parentElement.children].forEach((sib) =>
-      sib.classList.remove("active")
-    );
-    item.classList.add("active");
-    let cat = item.getAttribute("data-class");
-    console.log(cat);
-    let videos = document.querySelector(".videos-price");
-    let photos = document.querySelector(".photos-price");
-    if (cat == ".videos-price") {
-      photos.style.display = "none";
-      videos.style.display = "block";
-    } else {
-      photos.style.display = "block";
-      videos.style.display = "none";
-    }
-  });
+    item.addEventListener("click", function (e) {
+        // item.sibl.classList.remove('active')
+        // item.classList.add('active')
+        [...item.parentElement.children].forEach((sib) =>
+            sib.classList.remove("active")
+        );
+        item.classList.add("active");
+        let cat = item.getAttribute("data-class");
+        console.log(cat);
+        let videos = document.querySelector(".videos-price");
+        let photos = document.querySelector(".photos-price");
+        if (cat == ".videos-price") {
+            photos.style.display = "none";
+            videos.style.display = "block";
+        } else {
+            photos.style.display = "block";
+            videos.style.display = "none";
+        }
+    });
 });
 
 let albums = document.querySelectorAll(".albums .links ul li");
 if (albums) {
-  albums.forEach((item) => {
-    item.addEventListener("click", function () {
-      [...item.parentElement.children].forEach((sib) =>
-        sib.classList.remove("active")
-      );
-      item.classList.add("active");
-      let cat = item.getAttribute("data-class");
-      console.log(cat);
-      let allImages = document.querySelectorAll(".albums .albums-images");
-      allImages.forEach((item) => {
-        item.classList.add("d-none");
-      });
-      document
-        .querySelector(`.albums .albums-images${cat}`)
-        .classList.remove("d-none");
-      document
-        .querySelector(`.albums .albums-images${cat}`)
-        .classList.add("d-grid");
+    albums.forEach((item) => {
+        item.addEventListener("click", function () {
+            [...item.parentElement.children].forEach((sib) =>
+                sib.classList.remove("active")
+            );
+            item.classList.add("active");
+            let cat = item.getAttribute("data-class");
+            console.log(cat);
+            let allImages = document.querySelectorAll(".albums .albums-images");
+            allImages.forEach((item) => {
+                item.classList.add("d-none");
+            });
+            document
+                .querySelector(`.albums .albums-images${cat}`)
+                .classList.remove("d-none");
+            document
+                .querySelector(`.albums .albums-images${cat}`)
+                .classList.add("d-grid");
+        });
     });
-  });
 }
 
 let allImages = document.querySelectorAll(".albums .albums-images");
 allImages.forEach((item) => {
-  item.classList.add("d-none");
+    item.classList.add("d-none");
 });
 document
-  .querySelector(`.albums .albums-images.foods`)
-  .classList.remove("d-none");
+    .querySelector(`.albums .albums-images.foods`)
+    .classList.remove("d-none");
 document.querySelector(`.albums .albums-images.foods`).classList.add("d-grid");
+
+
+$(document).ready(function () {
+    $('.albums-images').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Artlanguage Agency</small>';
+            }
+        }
+    });
+});
